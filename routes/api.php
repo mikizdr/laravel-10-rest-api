5ae462732b179b514a58a343cdf5f28e55743216
Miroslav Zdravković <121615693+mikiqsense@users.noreply.github.com>
2023-09-25 11:44:07 +0200

    Send JSON response for missing resource

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\Auth\AuthController;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,10 +33,18 @@ Route::post('/v1/login', [AuthController::class, 'login'])->name('api.login');
  * Limiting sending requests to the endpoints.
  * Use authentication to protect routes (Sanctum is already preinstalled in the fresh Laravel installation).
  */
-Route::middleware(['throttle:10,1', 'auth:sanctum'])
-    ->prefix('v1')
-    ->name('api.')
+Route::middleware(['throttle:10,1', 'auth:sanctum']) // middlewares for throttling and authentication
+    ->prefix('v1') // API version
+    ->name('api.') // route name prefix
     ->group(function () {
-        Route::apiResource('products', ProductController::class);///->only(['index', 'show']);
+        // TODO: extract 'index' and 'show' method outside of protected routes if they should be public.
+        Route::apiResource('products', ProductController::class)
+            ->missing(function (Request $request): JsonResponse {
+                return response()->json([
+                    'message' => 'Product not found',
+                ], Response::HTTP_NOT_FOUND);
+            });
+
+        // Logout route.
         Route::post('logout', [AuthController::class, 'logout'])->name('logout');
     });
